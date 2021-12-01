4f5c356111c5ad5c4b60cbafa96930669e7abc03
“shuyvmuge” <1358644335@qq.com>
2021-12-01 20:47:06 +0800

    second commit

---
+++ pages/index/index.vue
@@ -0,0 +1,55 @@
+<template>
+	<u-swiper
+			:list="list1"
+			@change="change"
+			@click="click"
+	></u-swiper>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list1: [
+					'https://cdn.uviewui.com/uview/swiper/swiper1.png',
+					'https://cdn.uviewui.com/uview/swiper/swiper2.png',
+					'https://cdn.uviewui.com/uview/swiper/swiper3.png',
+				]
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
